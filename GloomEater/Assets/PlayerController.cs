b541e9535d0d74896aefabcd346ce4699a593351
nandanvinjamury <nvinjamury@gmail.com>
2019-08-10 12:25:16 -0700

    fixed unity version; testing shark movement

--- GloomEater/Assets/PlayerController.cs
+++ GloomEater/Assets/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Vector3 sharkMovementInput;
+    public Rigidbody sharkRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sharkRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementInput();
+    }
+
+
+    public void movementInput()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+        sharkMovementInput = new Vector3(movementX, 0, movementZ);
+
+
+        sharkRB.velocity = sharkMovementInput;
     }
 }
