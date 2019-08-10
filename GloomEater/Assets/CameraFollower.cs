1e808ce2735d2bd8ff2be70c2077af224bd0b0bd
nandanvinjamury <nvinjamury@gmail.com>
2019-08-10 13:03:32 -0700

    camera follow fix

--- GloomEater/Assets/CameraFollower.cs
+++ GloomEater/Assets/CameraFollower.cs
@@ -15,6 +15,6 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 0, -10);
+        transform.position = player.transform.position + new Vector3(0, 4, -10);
     }
 }
