ee8bdf63472007455500428518182c08d44eac24
nandanvinjamury <nvinjamury@gmail.com>
2019-08-10 12:54:50 -0700

    basic camera follow

---
+++ GloomEater/Assets/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Sharky");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 0, -10);
+    }
+}
