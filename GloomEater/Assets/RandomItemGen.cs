c936ad23313e4355ccc22971b5a92d42df454088
nandanvinjamury <nvinjamury@gmail.com>
2019-08-10 20:01:41 -0700

    edits to rotation/generation

--- GloomEater/Assets/RandomItemGen.cs
+++ GloomEater/Assets/RandomItemGen.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class RandomItemGen : MonoBehaviour
 {
-
-    float randomNumber;
+    public GameObject[] enemyList;
+    public GameObject[] collectibleList;
+    private float randomNumberX;
+    private float randomNumberY;
+    private float randomNumberZ;
+    private int relative;
+    private int randomItem;
 
 
     // Start is called before the first frame update
@@ -14,9 +19,79 @@ public class RandomItemGen : MonoBehaviour
         InvokeRepeating("Generate", 3f, 3f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Generate()
     {
-        
+        randomNumberX = Random.Range(5f, 10f);
+        randomNumberY = Random.Range(5f, 10f);
+        randomNumberZ = Random.Range(5f, 10f);
+        relative = Random.Range(0, 8);
+        randomItem = Random.Range(0, enemyList.Length);
+        switch (relative)
+        {
+            case 0:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(randomNumberX, randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(randomNumberX, randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(randomNumberX, -randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(randomNumberX, -randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(-randomNumberX, randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(-randomNumberX, randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 6:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(-randomNumberX, -randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 7:
+                Instantiate(enemyList[randomItem], transform.position + new Vector3(-randomNumberX, -randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            default:
+                break;
+
+        }
+
+        randomNumberX = Random.Range(5f, 10f);
+        randomNumberY = Random.Range(5f, 10f);
+        randomNumberZ = Random.Range(5f, 10f);
+        relative = Random.Range(0, 8);
+        randomItem = Random.Range(0, collectibleList.Length);
+        switch (relative)
+        {
+            case 0:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(randomNumberX, randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(randomNumberX, randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(randomNumberX, -randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(randomNumberX, -randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(-randomNumberX, randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(-randomNumberX, randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            case 6:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(-randomNumberX, -randomNumberY, randomNumberZ), Quaternion.identity);
+                break;
+            case 7:
+                Instantiate(collectibleList[randomItem], transform.position + new Vector3(-randomNumberX, -randomNumberY, -randomNumberZ), Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+
     }
+
 }
