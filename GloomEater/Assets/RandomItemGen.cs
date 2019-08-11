f0d9bf7594bb8aa160302e2cf7a191a25645d967
jgregfong <jgregfong@gmail.com>
2019-08-10 17:36:59 -0700

    Added meteor model and started RandomItemGen script.

---
+++ GloomEater/Assets/RandomItemGen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItemGen : MonoBehaviour
+{
+
+    int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i< number; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
